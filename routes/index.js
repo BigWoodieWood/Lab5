0a4cb7f9768b233b8cc3e396c68243571485c540
Elliot Wood <Elliot.waterpolo@outlook.com>
2025-10-06 19:05:04 -0400

    Ajout et supression, cas limite gérée

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,50 @@ var router = express.Router();
 const liste = require('../models/listeGlobale');
 
 router.get('/', function(req, res, next) { 
-    res.render('pages/accueil', { title: 'Accueil', items: liste.getItems() }); 
-}); 
+    let message = null;
+    res.render('pages/accueil', { title: 'Accueil', items: liste.getItems(), message });
+});
+router.post('/add', function(req, res, next) { 
+    liste.ajouterItem(req.body.nom, parseFloat(req.body.prix));
+    res.redirect('/');
+});
 
- 
+router.post('/deleteById', function(req, res, next) {
+    const id = parseInt(req.body.id, 10);
+    let message = null;
+    if (!id) {
+        message = "ID vide." ;
+    } else {
+        const item = liste.getItemById(id);
+        if (item) {
+            liste.removeItemById(id);
+            console.log(`Item avec ID "${id}" supprimé.`);
+        } else {
+            console.log(`Item avec ID "${id}" introuvable.`);
+            message = `Item "${id}" introuvable.` ;
+        }
+    }
+    res.render('pages/accueil', { title: 'Accueil', items: liste.getItems(), message });
+});
+
+router.post('/deleteByName', function(req, res, next) {
+    const nom = req.body.nom.trim();
+    let message = null;
+
+    if (!nom) {
+        message = "Nom vide.";
+    } else {
+        const item = liste.getItemByName(nom);
+        if (item) {
+            liste.removeItemByName(nom);
+            console.log(`Item avec nom "${nom}" supprimé.`);
+        } else {
+            console.log(`Item avec nom "${nom}" introuvable.`);
+            message = `Item "${nom}" introuvable.`;
+        }
+    }
+
+    res.render('pages/accueil', { title: 'Accueil', items: liste.getItems(), message });
+});
 
 module.exports = router;
